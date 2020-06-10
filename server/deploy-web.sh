af3685f46b3e6d07c504001bfa0d784d057f3a2b
Mark <jeff@thevisual.work>
2020-06-10 14:15:52 -0700

    Upgrade server deploy script to work with nvm and yarn.

--- server/deploy-web.sh
+++ server/deploy-web.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-## Jeff's deploy script v3.0
+## Jeff's deploy script v4.0
 ## Spencer Wohlers
 
 ## Here's what we're gonna do. Grab the latest copy of the portfolio from
-## Github. Rebuild it using npm. Swap out the directory.
-
-## Now, with more Apache.
+## Github. Rebuild it using yarn. Swap out the directory.
 
 GIT_BASE_DIR=/home/jeff
 GIT_SUBDIR=portfolio
@@ -17,6 +15,9 @@ CHOWN_USER=jeff
 CHOWN_GROUP=www-data
 GH_MASTER="https://github.com/gitmarkhubmunar/portfolio.git"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 if [ -d $GIT_BASE_DIR/$GIT_SUBDIR ]; then
   
@@ -32,24 +33,21 @@ else
   cd $GIT_BASE_DIR
   git clone $GH_MASTER $GIT_SUBDIR &>/dev/null
   cd $GIT_SUBDIR
-  npm install &>/dev/null
+  yarn install --ignore-engines &>/dev/null
   
 fi
-  
-echo "Building production-ready site."
-npm run build &>/dev/null
 
-# echo "Stopping existing server processes."
-# killall node &>/dev/null;
+echo "Installing/upgrading packages."
+cd $GIT_BASE_DIR/$GIT_SUBDIR
+yarn install --ignore-engines &>/dev/null
+
+echo "Building production-ready site."
+yarn build &>/dev/null
 
 echo "Moving updated site into place."
 chmod 775 build
 chown -R $CHOWN_USER:$CHOWN_GROUP build
 rm -rf $SRV_BASE_DIR/$SRV_SUBDIR
 mv build $SRV_BASE_DIR/$SRV_SUBDIR
-
-# echo "Starting server."
-# cd $SRV_BASE_DIR
-# /usr/local/bin/pushstate-server $SRV_SUBDIR &>/dev/null &
 
 exit 0;
